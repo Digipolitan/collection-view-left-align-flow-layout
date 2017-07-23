6fc4dfbc6d667f2376881575cff5c1fcaa26d1c2
Julien Sarazin <julien.sarazin@me.com>
2017-07-23 11:37:04 +0200

    fixed condition to determine if an attributes were representing a new row.

--- Sources/DGCollectionViewLeftAlignFlowLayout.swift
+++ Sources/DGCollectionViewLeftAlignFlowLayout.swift
@@ -52,7 +52,7 @@ public class DGCollectionViewLeftAlignFlowLayout: UICollectionViewFlowLayout {
 
 		let previousAttributes = self.layoutAttributesForItem(at: IndexPath(item: indexPath.item - 1, section: indexPath.section))
 		let previousFrame: CGRect = previousAttributes?.frame ?? CGRect()
-		let firstInRow = previousFrame.origin.y != attributes.frame.origin.y
+		let firstInRow = previousAttributes?.center.y != attributes.center.y
 
 		guard !firstInRow else {
 			let section = attributes.indexPath.section
