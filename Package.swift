32c7cdb00bb9f4922a6b27271399c1de6e4a1a25
Tobias Ullerich <tobias.ullerich@init.de>
2020-09-16 13:25:09 +0200

    Add swiftpm

--- Package.swift
+++ Package.swift
@@ -1,7 +1,24 @@
+// swift-tools-version:5.1
 import PackageDescription
 
 let package = Package(
-    name: "DGCollectionViewLeftAlignFlowLayout",
+    name: "collection-view-left-align-flow-layout",
+    platforms: [
+        .iOS(.v8)
+    ],
+    products: [
+        .library(
+            name: "collection-view-left-align-flow-layout",
+            targets: ["collection-view-left-align-flow-layout"]
+        )
+    ],
     dependencies: [
+        
+    ],
+    targets: [
+        .target(
+            name: "collection-view-left-align-flow-layout",
+            path: "Source"
+        )
     ]
 )
