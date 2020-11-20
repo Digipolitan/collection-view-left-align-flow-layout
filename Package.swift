bf22a749e7bb3155251c53a466615e183f32aea9
Tobias Ullerich <thinkdifferent055@gmail.com>
2020-11-20 10:45:41 +0100

    Update Package.swift
    Use correct library name in Package.swift

--- Package.swift
+++ Package.swift
@@ -2,14 +2,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "collection-view-left-align-flow-layout",
+    name: "DGCollectionViewLeftAlignFlowLayout",
     platforms: [
         .iOS(.v8)
     ],
     products: [
         .library(
-            name: "collection-view-left-align-flow-layout",
-            targets: ["collection-view-left-align-flow-layout"]
+            name: "DGCollectionViewLeftAlignFlowLayout",
+            targets: ["DGCollectionViewLeftAlignFlowLayout"]
         )
     ],
     dependencies: [
@@ -17,7 +17,7 @@ let package = Package(
     ],
     targets: [
         .target(
-            name: "collection-view-left-align-flow-layout",
+            name: "DGCollectionViewLeftAlignFlowLayout",
             path: "Sources"
         )
     ]
